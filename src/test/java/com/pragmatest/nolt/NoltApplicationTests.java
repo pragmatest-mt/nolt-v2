001480747f1f14b43cda8a4ed675589c95c9fe7a
clydevassallo <cooler364@gmail.com>
2021-07-01 23:26:48 +0200

    Nolt project initialization using spring initializr
    Configuration:
    - Maven
    - Java
    - Spring Boot 2.5.2
    - Jar
    - Java 11
    - Spring for Apache Kafka Streams dependency

---
+++ src/test/java/com/pragmatest/nolt/NoltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragmatest.nolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
