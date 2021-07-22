0f641502ab403a2af275da7bd77eb13b27708ec0
Andrea Mangion <andrea.mangion@gig.com>
2021-07-22 23:09:08 +0200

    Adding Spring boot annotation

--- gateway/src/main/java/com/pragmatest/nolt/gateway/RoutingGatewayApplication.java
+++ gateway/src/main/java/com/pragmatest/nolt/gateway/RoutingGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.pragmatest.nolt.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@SpringBootApplication
 public class RoutingGatewayApplication {
 
     public static void main(String[] args) {
