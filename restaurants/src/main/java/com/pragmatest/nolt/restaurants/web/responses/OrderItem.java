70a49ab7c770ce54ddbcf2de25273461194c3340
Andrea Mangion <andrea.mangion@gig.com>
2021-07-25 16:31:34 +0200

    Adding OrderItem model to be used in OrderResponse

---
+++ restaurants/src/main/java/com/pragmatest/nolt/restaurants/web/responses/OrderItem.java
@@ -0,0 +1,50 @@
+package com.pragmatest.nolt.restaurants.web.responses;
+
+public class OrderItem {
+    String orderId;
+    String menuItemId;
+    int quantity;
+    String notes;
+
+    public OrderItem(String orderId, String menuItemId, int quantity, String notes) {
+        this.orderId = orderId;
+        this.menuItemId = menuItemId;
+        this.quantity = quantity;
+        this.notes = notes;
+    }
+
+    public OrderItem() {
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getMenuItemId() {
+        return menuItemId;
+    }
+
+    public void setMenuItemId(String menuItemId) {
+        this.menuItemId = menuItemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
