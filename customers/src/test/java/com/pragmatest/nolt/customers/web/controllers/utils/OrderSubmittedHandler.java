a780195ee4f8474af370c9d8527cba91b2fc0be6
Andrea Mangion <andrea.mangion@gig.com>
2021-07-29 23:26:01 +0200

    Setting eventReceived to static

--- customers/src/test/java/com/pragmatest/nolt/customers/web/controllers/utils/OrderSubmittedHandler.java
+++ customers/src/test/java/com/pragmatest/nolt/customers/web/controllers/utils/OrderSubmittedHandler.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderSubmittedHandler {
 
-    OrderSubmittedEvent eventReceived = null;
+    static OrderSubmittedEvent eventReceived = null;
 
     @KafkaListener(
             topics = "${order.submitted.topic}",
