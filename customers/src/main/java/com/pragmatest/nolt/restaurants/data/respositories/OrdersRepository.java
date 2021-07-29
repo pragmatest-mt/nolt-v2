ed320a07d87f12cc8d28fdbecc71e9d60ecfa019
Andrea Mangion <andrea.mangion@gig.com>
2021-07-29 18:35:22 +0200

    Fixing package name

--- customers/src/main/java/com/pragmatest/nolt/restaurants/data/respositories/OrdersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pragmatest.nolt.restaurants.data.respositories;
-
-import com.pragmatest.nolt.restaurants.data.entities.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<OrderEntity, String> {
-    
-}
