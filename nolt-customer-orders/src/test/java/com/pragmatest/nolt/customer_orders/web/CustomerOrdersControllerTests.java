41c290bbd92bf4667f49f93a29de63914db2239c
clydevassallo <cooler364@gmail.com>
2021-08-16 21:28:24 +0200

    Add submit valid order test placeholder

---
+++ nolt-customer-orders/src/test/java/com/pragmatest/nolt/customer_orders/web/CustomerOrdersControllerTests.java
@@ -0,0 +1,16 @@
+package com.pragmatest.nolt.customer_orders.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class CustomerOrdersControllerTests {
+
+    @Test
+    public void testSubmitOrderValidOrder() {
+
+    }
+
+}
