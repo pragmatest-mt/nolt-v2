bd85be545e970b9bc5d83105bb3d0cbfad33648f
clydevassallo <cooler364@gmail.com>
2021-09-30 22:28:17 +0200

    Update CustomerOrdersController.java

--- nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/web/controllers/CustomerOrdersController.java
+++ nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/web/controllers/CustomerOrdersController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerOrdersController {
 
     @PostMapping("orders")
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.CREATED)
     String submit() {
         return "Submitted";
     }
