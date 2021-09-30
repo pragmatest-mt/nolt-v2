6d8f11f8f6a2fe6912d68d152a5d97e05138277e
Andrea Mangion <andrea.mangion@gig.com>
2021-09-30 23:32:25 +0200

    Adding request and response models

---
+++ nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/web/responses/SubmitOrderResponse.java
@@ -0,0 +1,21 @@
+package com.pragmatest.nolt.customer_orders.web.responses;
+
+public class SubmitOrderResponse {
+
+    private String orderId;
+
+    public SubmitOrderResponse(String id) {
+        this.orderId = id;
+    }
+
+    public SubmitOrderResponse() {
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
