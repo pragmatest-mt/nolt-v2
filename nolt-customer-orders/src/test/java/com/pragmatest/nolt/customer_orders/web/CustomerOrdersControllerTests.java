214940ffb14861f9a0959cf14a9788e7c3b23bfe
Andrea Mangion <andrea.mangion@gig.com>
2021-10-01 00:30:20 +0200

    Adding JUnit Assertions static import

--- nolt-customer-orders/src/test/java/com/pragmatest/nolt/customer_orders/web/CustomerOrdersControllerTests.java
+++ nolt-customer-orders/src/test/java/com/pragmatest/nolt/customer_orders/web/CustomerOrdersControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static com.pragmatest.nolt.customer_orders.helpers.Assertions.*;
 
 @SpringBootTest
