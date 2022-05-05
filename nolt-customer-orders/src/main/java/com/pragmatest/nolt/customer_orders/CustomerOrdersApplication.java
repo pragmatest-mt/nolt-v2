e7b9ae734c1fd5372acb407e7f81de0fccfca7dc
clydevassallo <cooler364@gmail.com>
2022-05-05 23:28:32 +0200

    feat: generate controllers

--- nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/CustomerOrdersApplication.java
+++ nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/CustomerOrdersApplication.java
@@ -3,8 +3,10 @@ package com.pragmatest.nolt.customer_orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CustomerOrdersApplication {
 
     public static void main(String[] args) throws InterruptedException {
